06069da5c44cac9c167260494ab2b0b1aa9e5da9
camillavalenzuela <camilladelune@gmail.com>
2025-05-11 18:22:49 -0300

    (fork) verificação_idade.py

--- verificação_idade.py
+++ verificação_idade.py
@@ -1,6 +1,14 @@
-#script de verficação de maior idade
-idade = int(input('digite a sua idade'))
-if idade >= 18:
-    print('maior de idade')
-elif idade < 18:
-   print('menor de idade')
+# Verificador de maioridade em Python
+
+try:
+    idade = int(input('Digite sua idade: '))
+    
+    if idade >= 18:
+        print('Você é maior de idade!')
+    elif idade > 0:
+        print('Você é menor de idade.')
+    else:
+        print('Idade inválida. Digite um número positivo.')
+        
+except ValueError:  # Se o usuário digitar texto em vez de número
+    print('Erro: Digite apenas números!')
